428636f5773b7b83d265a0ede7e95c1360ee6fb0
rupesh <rupesh@e42.ai>
2025-12-26 17:27:18 +0530

    fix: Hide Navbar on Landing Page for unauthenticated users

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -26,7 +26,6 @@ const Home = () => {
     if (!user) {
         return (
             <div className="min-h-screen bg-gray-50 flex flex-col">
-                <Navbar />
                 <main className="flex-grow">
                     <LandingPage />
                 </main>
